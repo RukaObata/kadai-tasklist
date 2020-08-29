50fb9896cbc6b85647a5af4fe8cb3d36098f821a
ruka.obata <rukaobata.for.git@gmail.com>
2020-08-29 01:59:22 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -45,6 +45,7 @@ class TasksController extends Controller
     public function store(Request $request)
     {
         $task = new Task;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
         
@@ -92,6 +93,7 @@ class TasksController extends Controller
     {
         $task = Task::findOrFail($id);
         
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
         
