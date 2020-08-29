72f5ad1d8a0e611d8c752f2e768449fb24b91428
ruka.obata <rukaobata.for.git@gmail.com>
2020-08-29 02:13:35 +0000

    set validate

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -44,6 +44,12 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        $request->validate([
+            'status' => 'required|max:10',
+            ]);
+        $request->validate([
+            'content' => 'required|max:255',
+            ]);
         $task = new Task;
         $task->status = $request->status;
         $task->content = $request->content;
@@ -91,6 +97,13 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $request->validate([
+            'status' => 'required|max:10',
+            ]);
+        $request->validate([
+            'content' => 'required|max:255',
+            ]);
+            
         $task = Task::findOrFail($id);
         
         $task->status = $request->status;
