0979c4315052bb8df033207057b7b92d2426d192
ruka.obata <rukaobata.for.git@gmail.com>
2020-08-31 05:20:00 +0000

    user login

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -1,24 +1,27 @@
 <header class="mb-4">
-            <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-                {{-- トップページへのリンク --}}
-                <a class="navbar-brand" href="/">TaskList</a>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        {{-- トップページへのリンク --}}
+        <a class="navbar-brand" href="/">TaskList</a>
 
-                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-                <div class="collapse navbar-collapse" id="nav-bar">
-                    <ul class="navbar-nav mr-auto"></ul>
-                    <ul class="navbar-nav">
-                        {{--タスク作成ページへのリンク --}}
-                        <li class="nav-item">{!! link_to_route('tasks.create', '新規タスクのリマインド', [], ['class' => 'nav-link']) !!}</li>
-                    </ul>
-                    <ul class="nav navbar-nav navbar-right">
+        <div class="collapse navbar-collapse" id="nav-bar">
+            <ul class="navbar-nav mr-auto"></ul>
+            <ul class="navbar-nav">
+                @if (Auth::check())
+                    {{--タスク作成ページへのリンク --}}
+                    <li class="nav-item">{!! link_to_route('tasks.create', 'New Task', [], ['class' => 'nav-link']) !!}</li>
+                    {{-- ログアウトへのリンク --}}
+                    <li class="nav-item">{!! link_to_route('logout.get', 'Logout') !!}</li>
+                @else
                     {{-- ユーザ登録ページへのリンク --}}
                     <li>{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
                     {{-- ログインページへのリンク --}}
-                    <li><a href="#">Login</a></li>
-                </ul>
-                </div>
-            </nav>
-        </header>
+                    <li class="nav-item">{!! link_to_route('login', 'Login', [], ['class' => 'nav-link']) !!}</li>
+                @endif
+            </ul>
+        </div>
+    </nav>
+</header>
